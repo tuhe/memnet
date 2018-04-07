4640583b14e2421ec02ebaf045347bec02159752
tuhe <tuhe@dtu.dk>
2018-04-07 12:22:59 -0700

    updates

--- mnist_cnn_base.py
+++ mnist_cnn_base.py
@@ -230,7 +230,7 @@ def main(unused_argv):
 
     # Set up logging for predictions
     # Log the values in the "Softmax" tensor with label "probabilities"
-    tensors_to_log = {"probabilities": "softmax_tensor"}
+    tensors_to_log = {} #{"probabilities": "softmax_tensor"}
     logging_hook = tf.train.LoggingTensorHook(
         tensors=tensors_to_log, every_n_iter=50)
 
